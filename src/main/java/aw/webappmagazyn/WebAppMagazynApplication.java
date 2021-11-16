ed62fceac5de5afa6415829b7e3d533998b13103
Adrian Wielgus <adrianwielgus99@gmail.com>
2021-11-16 13:07:36 +0100

    Initial commit

---
+++ src/main/java/aw/webappmagazyn/WebAppMagazynApplication.java
@@ -0,0 +1,13 @@
+package aw.webappmagazyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppMagazynApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppMagazynApplication.class, args);
+    }
+
+}
---
+++ src/test/java/aw/webappmagazyn/WebAppMagazynApplicationTests.java
@@ -0,0 +1,13 @@
+package aw.webappmagazyn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppMagazynApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
