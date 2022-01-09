77c2369a6794987ca004caa08b4d88d83e4c9596
AWielgus <adrianwielgus99@gmail.com>
2022-01-09 21:16:43 +0100

    Wprowadzono poprawki i zakończono pracę na product type( wymagany serwis i howanie danych. )

--- src/main/java/aw/webappmagazyn/controller/TypeController.java
+++ src/main/java/aw/webappmagazyn/controller/TypeController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.time.LocalDateTime;
@@ -43,5 +44,27 @@ public class TypeController {
         model.addAttribute("list",listOfTypes);
         return "type/typeList";
     }
+
+    @GetMapping("/typeList/remove/{id}")
+    public String typeRemove(@PathVariable Long id) {
+        productTypeRepository.deleteById(id);
+        return "redirect:/typeList";
+    }
+    @GetMapping("/typeList/edit/{id}")
+    public String typeEdit(@PathVariable Long id, Model model) {
+        model.addAttribute("productType",productTypeRepository.getById(id));
+        return "type/typeEdit";
+    }
+
+    @PostMapping("/typeList/update")
+    public String printerAdd(ProductType productType){
+        productType.setModificationDate(LocalDateTime.now());
+        productTypeRepository.save(productType);
+        return "redirect:/typeList";
+    }
+
+//    <select class="col-form-label col-4 " id="printer-ip-input">
+//         <option th:each="i : ${printers}" th:value="${i.ip +';'+ i.port}" th:text="${i.ip +' '+ i.name}">
+//    </select>
 
 }
--- src/main/java/aw/webappmagazyn/model/ProductType.java
+++ src/main/java/aw/webappmagazyn/model/ProductType.java
@@ -31,4 +31,5 @@ public class ProductType {
                 ", hidden=" + hidden +
                 '}';
     }
+
 }
